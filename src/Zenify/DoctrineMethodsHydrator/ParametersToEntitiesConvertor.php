96f4d4bda306efd227b79c96d46bb9e009c616cb
Petr Olišar <petr.olisar@gmail.com>
2014-09-28 18:22:12 +0200

    Support for custom find by methods

--- src/Zenify/DoctrineMethodsHydrator/ParametersToEntitiesConvertor.php
+++ src/Zenify/DoctrineMethodsHydrator/ParametersToEntitiesConvertor.php
@@ -28,7 +28,22 @@ class ParametersToEntitiesConvertor extends Nette\Object
 	/**
 	 * @return mixed[]
 	 */
-	public function process(array $methodParameters, array $args)
+	public function process(array $methodParameters, array $args, $rm)
+	{
+		if($rm->hasAnnotation('hydratorMethod') && $rm->hasAnnotation('hydratorDao'))
+		{
+			return $this->useCustomMethod($methodParameters, $args, $rm);
+		} else
+		{
+			return $this->useFindById($methodParameters, $args);
+		}
+	}
+	
+	
+	/**
+	 * @return mixed[]
+	 */
+	private function useFindById(array $methodParameters, array $args)
 	{
 		foreach ($methodParameters as $i => $parameter) {
 			if ($className = $parameter->className) {
@@ -37,7 +52,28 @@ class ParametersToEntitiesConvertor extends Nette\Object
 				}
 			}
 		}
-
+		return $args;
+	}
+	
+	
+	/**
+	 * @return mixed[]
+	 */
+	private function useCustomMethod(array $methodParameters, array $args, $rm)
+	{
+		$hydratatorMethod = $rm->getAnnotation('hydratorMethod');
+		$hydratatorDao = $rm->getAnnotation('hydratorDao');
+		foreach ($methodParameters as $i => $parameter) {
+			if ($parameter->className || $rm->hasAnnotation('hydratatorEntity')) {
+				$className = !is_null($parameter->className) ? 
+					$parameter->className : $rm->getAnnotation('hydratatorEntity');
+				if ($this->isEntity($className) && $args[$i] !== NULL && $args[$i] !== FALSE) {
+					$dao = new $hydratatorDao($this->em);
+					$entity = $dao->$hydratatorMethod($args[$i]);
+					$args[$i] = $this->checkEntity($entity);
+				}
+			}
+		}
 		return $args;
 	}
 
@@ -72,6 +108,25 @@ class ParametersToEntitiesConvertor extends Nette\Object
 		} catch (Doctrine\Common\Persistence\Mapping\MappingException $e) {
 			return FALSE;
 		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param array $entity
+	 * @return array
+	 * @throws BadRequestException
+	 */
+	private function checkEntity(array $entity)
+	{
+		if (!count($entity)) {
+			throw new BadRequestException('Entity "' . $className . '" with id = "' . $args[$i] . '" was not found.');
+		}
+		if(count($entity) === 1)
+		{
+			$entity = $entity[0];
+		}
+		return $entity;
 	}
 
 }
