6f44338f303389c1cb4ea11912525014d47450b8
Tomas Votruba <tomas.vot@gmail.com>
2015-06-10 19:24:17 +0200

    DI: load from services.neon (Zenify standard)

--- src/DI/MethodsHydratorExtension.php
+++ src/DI/MethodsHydratorExtension.php
@@ -8,8 +8,6 @@
 namespace Zenify\DoctrineMethodsHydrator\DI;
 
 use Nette\DI\CompilerExtension;
-use Zenify\DoctrineMethodsHydrator\Doctrine\ParametersToEntitiesConvertor;
-use Zenify\DoctrineMethodsHydrator\MethodsHydrator;
 
 
 class MethodsHydratorExtension extends CompilerExtension
@@ -17,13 +15,9 @@ class MethodsHydratorExtension extends CompilerExtension
 
 	public function loadConfiguration()
 	{
-		$builder = $this->getContainerBuilder();
-
-		$builder->addDefinition($this->prefix('hydrator'))
-			->setClass(MethodsHydrator::class);
-
-		$builder->addDefinition($this->prefix('convertor'))
-			->setClass(ParametersToEntitiesConvertor::class);
+		$containerBuilder = $this->getContainerBuilder();
+		$services = $this->loadFromFile(__DIR__ . '/services.neon');
+		$this->compiler->parseServices($containerBuilder, $services);
 	}
 
 }
